46aa85467f49ac686157763d79e18c0b940edaff
YoussefG75 <youssef.gasmii1@gmail.com>
2023-07-30 03:02:11 +0200

    New code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className='TodoForm'>
+      <input type='text' className='todo-input' placeholder='What is the tast today?' />
+      <button type='submit' className='todo-btn'></button>
+    </form>
   );
 }
 

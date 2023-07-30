86fc402786c61e533ff0329907fa0201ce4564fa
YoussefG75 <youssef.gasmii1@gmail.com>
2023-07-30 10:43:42 +0200

    New changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { TodoWrapper } from './components/TodoWrapper';
 
 function App() {
   return (
-    <form className='TodoForm'>
-      <input type='text' className='todo-input' placeholder='What is the tast today?' />
-      <button type='submit' className='todo-btn'></button>
-    </form>
+    <div className='App'>
+      <TodoWrapper />
+    </div>
   );
 }
 
